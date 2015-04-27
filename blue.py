9b36c057c5e7ee989b72aee3efadf12e6432b1e5
stcalica <kycalica@gmail.com>
2015-04-27 11:10:04 -0700

    clear button gui made

--- blue.py
+++ blue.py
@@ -15,10 +15,14 @@ class Form(GridLayout):
 		self.add_widget(self.store)
 		self.add_widget(Label(text="Item"))
 		self.item = TextInput(multiline=False)
-		self.add_widget(Label(text="Store"))
+		self.add_widget(self.item)
 		self.add_widget(Button(text="Broadcast Sale", on_press=self.submit))
+		self.add_widget(Button(text="Clear", on_press=self.clear))
 	def submit(self, *args):
 		print("button pressed")
+		
+	def clear(self, *args):
+		print("clear is pressed")
 
 #actuall app
 class BlueTagApp(App):
